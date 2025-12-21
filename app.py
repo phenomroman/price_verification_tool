4019114d4ebd15c944e1574c4ea87d9af3ec576b
phenomroman <phenomroman@gmail.com>
2025-12-22 03:22:26 +0600

    Refined country list and rearranged some input fields.

--- app.py
+++ app.py
@@ -21,11 +21,13 @@ for filename in os.listdir("price_models"):
         code = filename.replace(".pkl", "")
         pipelines[code] = joblib.load(os.path.join("price_models", filename))
 
-st.set_page_config(page_title="Price Verification Tool 🏷️", layout="centered")
-
-st.title("💰 Unit Price Assessment")
-st.markdown("Use this tool to assess potential under/over-invoicing based on historical import data.")
-
+# --- Input Variables ---
+country_list = ['AFGHANISTAN', 'ALGERIA', 'ARGENTINA', 'AUSTRALIA', 'AUSTRIA', 'BANGLADESH', 'BELARUS', 'BELGIUM', 'BENIN', 'BHUTAN', 'BOSNIA AND HERZEGOVINA', 'BRAZIL', 'BULGARIA', 'BURKINA FASO', 'CAMBODIA', 'CAMEROON', 'CANADA', 'CHAD', 'CHILE', 'CHINA, PEOPLE’S REPUBLIC OF', 'CONGO', 'CROATIA', 'CZECH REPUBLIC', 'CZECH REPUBLIC ', 'DENMARK', 'ECUADOR', 'EGYPT', 'EL-SALVADOR', 'ESTONIA', 'EUROPE', 'FIJI', 'FINLAND', 'FRANCE', 'GERMANY', 'GREECE', 'GUYANA', 'HONGKONG', 'HUNGARY', 'ICELAND', 'INDIA', 'INDONESIA', 'IRAQ', 'IRELAND', 'ITALY', 'JAPAN', 'KIRIBATI, REPUBLIC OF', 'KOREA, REPUBLIC OF (SOUTH)', 'KUWAIT', 'KYRGYZSTAN', 'LATVIA', 'LEBANON', 'LIBERIA', 'LITHUANIA', 'LUXEMBOURG', 'MACAO', 'MACEDONIA', 'MADAGASCAR', 'MALAYSIA', 'MALI', 'MALTA', 'MAURITIUS', 'MEXICO', 'MOROCCO', 'MYANMAR', 'NAURU', 'NEPAL', 'NETHERLANDS', 'NEWZEALAND', 'NORWAY', 'OMAN', 'PAKISTAN', 'PANAMA', 'PAPUA NEW GUINEA', 'PARAGUAY', 'PERU', 'PHILIPPINES', 'POLAND', 'PORTUGAL', 'QATAR', 'ROMANIA', 'RUSSIAN FEDERATION', 'SAMOA', 'SAN MARINO', 'SAUDI ARABIA', 'SENEGAL', 'SERBIA', 'SIERRA LEONE', 'SINGAPORE', 'SLOVAK REPUBLIC', 'SLOVENIA', 'SOUDIA ARABIA', 'SOUTH AFRICA', 'SOUTH SUDAN', 'SPAIN', 'SRI-LANKA', 'SUADIA ARABIA', 'SWEDEN', 'SWITZERLAND', 'TAIWAN', 'TAJIKISTAN', 'TANZANIA', 'THAILAND', 'TOGO', 'TUNISIA', 'TURKEY', 'U.A.E.', 'U.K', 'U.S.A', 'UKRAINE', 'URUGUAY', 'VANUATU', 'VIETNAM', 'ZIMBABWE']
+default_country = 'BANGLADESH'
+try:
+    default_country_index = country_list.index(default_country)
+except ValueError:
+    default_country_index = 0
 today = datetime.now()
 goods_info = {'52094200': 'FABRICS - WOVEN DENIM', 
               '58071000': 'ACCESSORIES - BADGE LABEL', 
@@ -40,32 +42,35 @@ goods_info = {'52094200': 'FABRICS - WOVEN DENIM',
               '96062200': 'ACCESSORIES - METAL BUTTON',
               '60041000': 'FABRICS - KNITTED'}
 
+# Initiate webpage
+st.set_page_config(page_title="Price Verification Tool 🏷️", layout="centered")
+st.title("💰 Unit Price Assessment")
+st.markdown("Use this tool to assess potential under/over-invoicing based on historical import data.")
+
 # --- Input Fields ---
 col1, col2 = st.columns(2)
 with col1:
     goods_code = st.selectbox("Goods Code", list(pipelines.keys() if pipelines else models.keys()))
-    exporter = st.text_input("Exporter")
-    exporter_country = st.selectbox("Exporter Country", 
-                                    options=["AFGHANISTAN", "ALGERIA", "ARGENTINA", "AUSTRALIA", "AUSTRIA", "BANGLADESH", "BELARUS", "BELGIUM", "BENIN", "BHUTAN", "BOSNIA AND HERZEGOVINA", "BRAZIL", "BULGARIA", "BURKINA FASO", "CAMBODIA", "CAMEROON", "CANADA", "CHAD", "CHILE", "CHINA PEOPLE'S REPUBLIC (P.R)", "CONGO", "CROATIA", "CZECH REPUBLIC", "DENMARK", "ECUADOR", "EGYPT", "ESTONIA", "FIJI", "FINLAND", "FRANCE", "GERMANY", "GREECE", "GUYANA", "HONG KONG, SAR OF CHINA", "HUNGARY", "INDIA", "INDONESIA", "IRAQ", "IRELAND", "ITALY", "JAPAN", "KOREA, REPUBLIC OF", "KUWAIT", "KYRGYZSTAN", "LATVIA", "LEBANON", "LUXEMBOURG", "MACEDONIA", "MADAGASCAR", "MALAYSIA", "MALI", "MALTA", "MEXICO", "MOROCCO", "MYANMAR", "NAURU", "NEPAL", "NETHERLANDS", "NEW ZEALAND", "NORWAY", "OMAN", "PAKISTAN", "PAPUA NEW GUINEA", "PARAGUAY", "PERU", "PHILIPPINES", "POLAND", "PORTUGAL", "QATAR", "ROMANIA", "RUSSIAN FEDERATION", "SAN MARINO", "SAUDI ARABIA", "SERBIA", "SIERRA LEONE", "SINGAPORE", "SLOVAKIA", "SLOVENIA", "SOUTH AFRICA", "SPAIN", "SRI LANKA", "SWEDEN", "SWITZERLAND", "TAIWAN", "TAJIKISTAN", "TANZANIA", "THAILAND", "TOGO", "TUNISIA", "TURKEY", "U.S.A.", "UK", "UKRAINE", "UNITED ARAB EMIRATES (UAE)", "URUGUAY", "VANUATU", "VIETNAM", "ZIMBABWE"])
-    importer = st.text_input("Importer")
-    origin_country = st.selectbox("Country of Origin",
-                                  options=["AFGHANISTAN", "ALGERIA", "ARGENTINA", "AUSTRALIA", "AUSTRIA", "BANGLADESH", "BELARUS", "BELGIUM", "BENIN", "BHUTAN", "BOSNIA AND HERZEGOVINA", "BRAZIL", "BULGARIA", "BURKINA FASO", "CAMBODIA", "CAMEROON", "CANADA", "CHAD", "CHILE", "CHINA PEOPLE'S REPUBLIC (P.R)", "CONGO", "CROATIA", "CZECH REPUBLIC", "DENMARK", "ECUADOR", "EGYPT", "ESTONIA", "FIJI", "FINLAND", "FRANCE", "GERMANY", "GREECE", "GUYANA", "HONG KONG, SAR OF CHINA", "HUNGARY", "INDIA", "INDONESIA", "IRAQ", "IRELAND", "ITALY", "JAPAN", "KOREA, REPUBLIC OF", "KUWAIT", "KYRGYZSTAN", "LATVIA", "LEBANON", "LUXEMBOURG", "MACEDONIA", "MADAGASCAR", "MALAYSIA", "MALI", "MALTA", "MEXICO", "MOROCCO", "MYANMAR", "NAURU", "NEPAL", "NETHERLANDS", "NEW ZEALAND", "NORWAY", "OMAN", "PAKISTAN", "PAPUA NEW GUINEA", "PARAGUAY", "PERU", "PHILIPPINES", "POLAND", "PORTUGAL", "QATAR", "ROMANIA", "RUSSIAN FEDERATION", "SAN MARINO", "SAUDI ARABIA", "SERBIA", "SIERRA LEONE", "SINGAPORE", "SLOVAKIA", "SLOVENIA", "SOUTH AFRICA", "SPAIN", "SRI LANKA", "SWEDEN", "SWITZERLAND", "TAIWAN", "TAJIKISTAN", "TANZANIA", "THAILAND", "TOGO", "TUNISIA", "TURKEY", "U.S.A.", "UK", "UKRAINE", "UNITED ARAB EMIRATES (UAE)", "URUGUAY", "VANUATU", "VIETNAM", "ZIMBABWE"])
+    exporter_country = st.selectbox("Exporter Country", options=country_list, index=default_country_index)
+    shipment_from = st.selectbox("Shipment From Port/Country", options=country_list, index=default_country_index)
+    trade_year = st.number_input("Trading Year", min_value=2022, step=1, max_value=today.year-1 if today.month==1 else today.year)
     currency = st.selectbox("Currency (e.g., USD, EUR)",  
                             options=['USD', 'EUR', 'JPY', 'GBP', 'AUD', 'CAD', 'CHF', 'CNY', 'INR'])
     incoterm = st.selectbox("Incoterm (e.g., FOB, CPT)", 
                             options=['EXW', 'FCA', 'CPT', 'CIP', 'DAP (DAF, DES, DDU)', 'DPU', 'DDP', 'FAS', 'CIF', 'FOB', 'CNF (CFR)'])
+    exporter = st.text_input("Exporter")
 with col2:
     goods_description = goods_info.get(goods_code, "No description available.")
     st.text_input(label="Goods Description", value=goods_description, disabled=True)
-    shipment_from = st.selectbox("Shipment From Port/Country", 
-                                 options=["AFGHANISTAN", "ALGERIA", "ARGENTINA", "AUSTRALIA", "AUSTRIA", "BANGLADESH", "BELARUS", "BELGIUM", "BENIN", "BHUTAN", "BOSNIA AND HERZEGOVINA", "BRAZIL", "BULGARIA", "BURKINA FASO", "CAMBODIA", "CAMEROON", "CANADA", "CHAD", "CHILE", "CHINA PEOPLE'S REPUBLIC (P.R)", "CONGO", "CROATIA", "CZECH REPUBLIC", "DENMARK", "ECUADOR", "EGYPT", "ESTONIA", "FIJI", "FINLAND", "FRANCE", "GERMANY", "GREECE", "GUYANA", "HONG KONG, SAR OF CHINA", "HUNGARY", "INDIA", "INDONESIA", "IRAQ", "IRELAND", "ITALY", "JAPAN", "KOREA, REPUBLIC OF", "KUWAIT", "KYRGYZSTAN", "LATVIA", "LEBANON", "LUXEMBOURG", "MACEDONIA", "MADAGASCAR", "MALAYSIA", "MALI", "MALTA", "MEXICO", "MOROCCO", "MYANMAR", "NAURU", "NEPAL", "NETHERLANDS", "NEW ZEALAND", "NORWAY", "OMAN", "PAKISTAN", "PAPUA NEW GUINEA", "PARAGUAY", "PERU", "PHILIPPINES", "POLAND", "PORTUGAL", "QATAR", "ROMANIA", "RUSSIAN FEDERATION", "SAN MARINO", "SAUDI ARABIA", "SERBIA", "SIERRA LEONE", "SINGAPORE", "SLOVAKIA", "SLOVENIA", "SOUTH AFRICA", "SPAIN", "SRI LANKA", "SWEDEN", "SWITZERLAND", "TAIWAN", "TAJIKISTAN", "TANZANIA", "THAILAND", "TOGO", "TUNISIA", "TURKEY", "U.S.A.", "UK", "UKRAINE", "UNITED ARAB EMIRATES (UAE)", "URUGUAY", "VANUATU", "VIETNAM", "ZIMBABWE"])
+    origin_country = st.selectbox("Country of Origin", options=country_list, index=default_country_index)
     shipment_to = st.selectbox("Shipment To Port/Country", 
                                options=['EPZ', 'CHITTAGONG', 'BENAPOL', 'Burimari', 'Others', 'SHAHAJALAL INT. AIRPORT'])
-    trade_year = st.number_input("Trading Year", min_value=2022, step=1, max_value=today.year-1 if today.month==1 else today.year)
     quantity = st.number_input("Quantity", min_value=0.0, step=0.1)
     tenor = st.number_input("Tenor of Payment", min_value=0, step=1)
     freight = st.number_input("Freight Charge", min_value=0.0, step=0.1)
-# Construct feature array – adapt according to your model’s training schema
+    importer = st.text_input("Importer")
+
+# Construct feature array – set according to model’s training schema
 input_array = np.array([[trade_year, quantity, tenor, freight, exporter, exporter_country, 
                 importer, origin_country, currency, incoterm, shipment_from, shipment_to]],
                 dtype=object)
@@ -76,6 +81,7 @@ cat_features = ["EXPORTER", "EXPORTER'S COUNTRY", "IMPORTER", "COUNTRY_OF_ORIGIN
 all_features = year_features + num_features + cat_features
 input_df = pd.DataFrame(data=input_array, columns=all_features)
 
+# Output on button press
 if st.button("🔍 Predict Unit Price"):
     # Model without data preprocessing
     model = models.get(goods_code)
@@ -89,14 +95,16 @@ if st.button("🔍 Predict Unit Price"):
     lower_bound = predicted_price * 0.85
     upper_bound = predicted_price * 1.15
 
+    # Show output
     st.success(f"**Predicted Unit Price:** {currency} {predicted_price:,.2f}")
     st.info(f"Expected Range: {currency} {lower_bound:,.2f} – {upper_bound:,.2f}")
-
     st.write("Use this range to detect under/over invoicing against declared unit price.")
     st.markdown("---")
     if goods_code == '58071000' or goods_code == '96061000':
         st.markdown(":red[!!! IMPORTANT !!! The prediction may not be accurate:]")
         st.write(f"Goods '{goods_code}: {goods_description}' did not have sufficient data for training.")
+
+# Styling
 st.markdown(
     """
     <style>
